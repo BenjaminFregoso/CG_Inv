13f1875f1d79e32b024ab99ccb11f5d9020fa465
humbertoaguila <48108038+humbertoaguila@users.noreply.github.com>
2019-07-11 13:24:05 -0500

    Revert "Pruebas Server"
    This reverts commit 9d711c9de0df46ddfa3595930d263cf94ad28f93.

--- CG_InvWeb/Nuevo_Arti.aspx.cs
+++ CG_InvWeb/Nuevo_Arti.aspx.cs
@@ -19,11 +19,8 @@ namespace CG_InvWeb {
     {
         public string cCodigo = "";
         public Int32 nCodigo = 0;
-        public string dato;
         protected void Page_Load(object sender, EventArgs e)
         {
-            dato = Server.MapPath("");
-
 
         }
 
