b04d862e4ab8d0fd4330535f0baf7ce0851f413c
Benjamin Fregoso <48106989+BenjaminFregoso@users.noreply.github.com>
2019-07-16 10:38:42 -0500

    Revert "Merge branch 'Benjamin'"
    This reverts commit aa716cf63b379f8789299ca59f10146467af1c79, reversing
    changes made to 2c2588d2653f8ff9346b3c8d601c328c54af3e99.

--- CG_InvWeb/Global.asax.cs
+++ CG_InvWeb/Global.asax.cs
@@ -7,9 +7,7 @@ using System;
     using DevExpress.Web;
 
     namespace CG_InvWeb {
-    
         public class Global_asax : System.Web.HttpApplication {
-        
             void Application_Start(object sender, EventArgs e) {
                 DevExpress.Web.ASPxWebControl.CallbackError += new EventHandler(Application_Error);
             }
@@ -19,25 +17,12 @@ using System;
             }
 
             void Application_Error(object sender, EventArgs e) {
-            var usuarios = System.Web.HttpContext.Current.Session["Usuario"];
-
-            Exception exception = Server.GetLastError();
-            Response.Clear();
-
-            HttpException httpException = exception as HttpException;
-
-            if (httpException != null)
-            {
-                string ultimoError = exception.Message;
-                GlobalHandler obje = new GlobalHandler();
-                obje.guardarLog(ultimoError);
+                // Code that runs when an unhandled error occurs
             }
-        }
 
             void Session_Start(object sender, EventArgs e) {
-            // Code that runs when a new session is started
-            //string usuario = System.Web.HttpContext.Current.Session["Usuario"].ToString();
-        }
+                // Code that runs when a new session is started
+            }
 
             void Session_End(object sender, EventArgs e) {
                 // Code that runs when a session ends. 
